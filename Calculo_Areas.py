1f4b02e02136003b76269cae916d4a53b16ce6ca
BrandsonG <44496863+BrandsonG@users.noreply.github.com>
2018-11-13 13:42:07 +0100

    Update Calculo_Areas.py

--- Calculo_Areas.py
+++ Calculo_Areas.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 opcio=input("Que quieres calcular T o C:")
 if(opcio=="T"):
-    base_triangulo=int(input("Base triangulo:"))
+    base_triangulo=float(input("Base triangulo:"))
     altura_triangulo=float(input("Altura triangulo:"))
     Res=abs(base_triangulo*altura_triangulo/2)
     print("El área es",Res)
@@ -11,3 +11,5 @@ else:
     Res_2=abs(2*pi*radio_circulo)
     if(opcio=="C"):
         print("Area del circulo es",Res_2)
+    else:
+        print("ERROR")
