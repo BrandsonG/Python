bf64a9ecb552823235ccd00d80feebbdf92bed60
BrandsonG <44496863+BrandsonG@users.noreply.github.com>
2018-11-15 10:37:44 +0100

    Create Leer_Dos_Numeros_Mostrar_Mayor.py

---
+++ Leer_Dos_Numeros_Mostrar_Mayor.py
@@ -0,0 +1,7 @@
+num_1 = float(input("Introduce un numero:"))
+num_2 = float(input("Introduce otro numero:"))
+
+if (num_1>num_2):
+    print("El mas grande es", num_1);
+else:
+    print("El mas grande es", num_2);
