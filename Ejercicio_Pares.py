645c616f2057b33dce1ed853c257d6ca683ae9b2
BrandsonG <44496863+BrandsonG@users.noreply.github.com>
2018-11-20 09:26:57 +0100

    Update Ejercicio_Pares.py

--- Ejercicio_Pares.py
+++ Ejercicio_Pares.py
@@ -1,7 +1,7 @@
 #No me sale
 Numero = float(input("Introduce un numero:"))
 
-if (Numero%2+1) :
+if (Numero%5) :
     print("Es par, que bonito numero par")
 else:
     print("Es impar, que numero mas vulgar")
